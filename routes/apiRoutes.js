eb61c0a767de54f9bf33a30a6108f858318ac987
Joe Rafalak <jrafalak@quickbase.com>
2020-01-28 20:51:49 -0500

    remove before_install for testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
-    })
+    });
   });
 
   // Create a new example
@@ -17,7 +17,9 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
